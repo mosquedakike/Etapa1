4413a995a562f2663e4a24730d2087e2fe0667dd
mosquedakike <enrique_inf@hoptmail.com>
2020-01-03 13:09:39 -0600

    Refactorizando proyecto con EscuelaEngine

---
+++ Etapa1/App/EscuelaEngine.cs
@@ -0,0 +1,33 @@
+﻿using CoreEscuela.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreEscuela
+{
+    public class EscuelaEngine
+    {
+        public Escuela Escuela { get; set; }
+
+        public EscuelaEngine()
+        {
+            
+        }
+
+        public void Inicializar()
+        {
+            Escuela = new Escuela("UPVM", 2009, TiposEscuela.Secundaria, "Mexico", "CDMX");
+
+            //Coleccion 
+            Escuela.CursosConList = new List<Curso>()
+            {
+                new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana},
+                new Curso(){ Nombre = "201", Jornada = TiposJornada.Mañana},
+                new Curso(){ Nombre = "301", Jornada = TiposJornada.Mañana},
+                new Curso(){ Nombre = "401", Jornada = TiposJornada.Tarde},
+                new Curso(){ Nombre = "501", Jornada = TiposJornada.Tarde}
+            };
+
+        }
+    }
+}
--- Etapa1/Program.cs
+++ Etapa1/Program.cs
@@ -4,92 +4,18 @@ using CoreEscuela.Entidades;
 using static System.Console;
 
 
-namespace Etapa1
+namespace CoreEscuela
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("UPVM",2009,TiposEscuela.Secundaria,"Mexico","CDMX");
-            WriteLine(escuela);
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
 
-            var escuela2 = new Escuela("UNAM", 1800, TiposEscuela.Universidad, "Mexico", "CDMX");
-
-            //Declaración de colecciones
-
-            //Coleccion 
-            escuela.CursosConList = new List<Curso>()
-            {
-                new Curso(){ Nombre = "101", Jornada = TiposJornada.Mañana},
-                new Curso(){ Nombre = "101", Jornada = TiposJornada.Tarde},
-                new Curso(){ Nombre = "301", Jornada = TiposJornada.Noche}
-            };
-
-            //Coleccion con [AddRange]
-            var otraColeccion = new List<Curso>{
-                new Curso{Nombre="101010", Jornada = TiposJornada.Noche}
-            };
-
-            //Array
-            escuela.CursosConArray = new Curso[]{
-                new Curso{ Nombre = "401"},
-                new Curso{ Nombre = "501"},
-                new Curso{ Nombre = "601", Jornada = TiposJornada.Tarde}
-            };
-
-            //Agregando nuevos Cursos a la coleccion [CursosConList]
-            escuela.CursosConList.Add(new Curso { Nombre="102", Jornada = TiposJornada.Tarde});
-            escuela.CursosConList.Add(new Curso { Nombre="10", Jornada = TiposJornada.Noche});
-
-            //otraColeccion.Clear();
-            escuela.CursosConList.AddRange(otraColeccion);
-
-            Curso temp = new Curso
-            {
-                Nombre="Ingles", Jornada=TiposJornada.Mañana
-            };
-
-            //Agregando un nuevo objeto a la lista [CursosConList]
-            escuela.CursosConList.Add(temp);
-
-            ImprimirCursosConArreglos(escuela);
-            ImprimirCursosConColecciones(escuela);
-
-            //Removiendo el objeto [temp] de la coleccion [CursosConList]
-            escuela.CursosConList.Remove(temp);
-
-            //Removiendo el objeto [obj] que cumpla con la condicion del metodo [predicado]
-            //escuela.CursosConList.RemoveAll(Predicado);
-            escuela.CursosConList.RemoveAll(delegate (Curso cur)
-            {
-                return cur.Nombre == "301";
-            });
-
-            escuela.CursosConList.RemoveAll((cur)=>cur.Nombre=="101" && cur.Jornada == TiposJornada.Mañana);
-
-            ImprimirCursosConColecciones(escuela);
-            //El HashCode es una propiedad que tienen todos los objetos y es un identificador que se les asigna
-            WriteLine(escuela.CursosConArray[0].GetHashCode());
-            WriteLine(escuela.CursosConArray[1].GetHashCode());
-
-            Console.ReadLine();
+            ImprimirCursosConColecciones(engine.Escuela);
         }
-
-        private static bool Predicado(Curso obj)
-        {
-            return obj.Nombre == "301";
-        }
-
-        private static void ImprimirCursosConArreglos(Escuela escuela)
-        {
-            WriteLine("*******************************************");
-            WriteLine("Cursos de la escuela con Arraglos");
-            WriteLine("*******************************************");
-            foreach (var curso in escuela.CursosConArray)
-            {
-                WriteLine($"Nombre: {curso.Nombre} Jornada: {curso.Jornada} ID: {curso.UniqId}");
-            }
-        }
+        
 
         public static void ImprimirCursosConColecciones(Escuela escuela)
         {
